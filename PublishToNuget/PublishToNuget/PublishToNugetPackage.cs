cfe65e7eb4633e9a398df272f17cbfef9b0a6142
wulang <wulang@dongzouxizou.com>
2021-12-21 10:17:26 +0800

    更新

--- PublishToNuget/PublishToNuget/PublishToNugetPackage.cs
+++ PublishToNuget/PublishToNuget/PublishToNugetPackage.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.Shell;
+using PublishToNuget.Tools;
 using System;
 using System.Runtime.InteropServices;
 using System.Threading;
@@ -46,6 +47,7 @@ namespace PublishToNuget
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await NuGetPublish.InitializeAsync(this);
         }
 
         #endregion
